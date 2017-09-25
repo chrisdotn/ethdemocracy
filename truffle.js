767e4eb2c8aa649081bc123c7ef4f273ff1f6c09
christophn <christoph.niemann@maibornwolff.de>
2017-09-25 14:33:03 +0200

    Format test reuslts for junit.xml

--- truffle.js
+++ truffle.js
@@ -1,9 +1,12 @@
 module.exports = {
-  networks: {
-    development: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*" // Match any network id
+    networks: {
+        development: {
+            host: "localhost",
+            port: 8545,
+            network_id: "*" // Match any network id
+        }
+    },
+    mocha: {
+        reporter: 'mocha-junit-reporter'
     }
-  }
 };

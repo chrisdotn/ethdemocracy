18f2b4bd78b9a9433542624da0d0ee26694489b8
christophn <christoph.niemann@maibornwolff.de>
2017-09-29 08:50:46 +0200

    Add more command from training

--- truffle.js
+++ truffle.js
@@ -5,8 +5,8 @@ module.exports = {
             port: 8545,
             network_id: "*" // Match any network id
         }
-    },
-    mocha: {
-        reporter: 'mocha-junit-reporter'
     }
+    // mocha: {
+    //     reporter: 'mocha-junit-reporter'
+    // }
 };

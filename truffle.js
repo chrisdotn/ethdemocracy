cf0a0d67e287535034986f9203722bb0669d6d10
Maximilian Pudelko <maximilian.pudelko@maibornwolff.de>
2017-11-21 16:59:46 +0100

    Use supplied testrpc instead of container

--- truffle.js
+++ truffle.js
@@ -4,10 +4,14 @@ module.exports = {
             host: "localhost",
             port: 8545,
             network_id: "*", // Match any network id
-            gas: 4600000
         }
-    }
-    ,
+    },
+    solc: {
+        optimizer: {
+            enabled: true,
+            runs: 200,
+        }
+    },
     // Uncomment this if you want XML test reports, leave as is for console output
     /*
     mocha: {
